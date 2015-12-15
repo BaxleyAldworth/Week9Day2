82910bb4aa346e944ac94ae20cd09267ff1631a7
BaxleyAldworth <jbaxle@gmail.com>
2015-12-15 14:32:04 -0600

    Updated AccountViewModels

--- Week9Day2.Auth/Models/AccountViewModels.cs
+++ Week9Day2.Auth/Models/AccountViewModels.cs
@@ -79,6 +79,18 @@ namespace Week9Day2.Auth.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Birth")]
+        public string BirthDate { get; set; }
     }
 
     public class ResetPasswordViewModel
