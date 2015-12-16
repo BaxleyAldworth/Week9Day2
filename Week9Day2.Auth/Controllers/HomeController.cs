1c0aa4d587eeade38c1a6c576dcc205d8ad930a5
BaxleyAldworth <jbaxle@gmail.com>
2015-12-16 17:27:04 -0600

    Home controller

--- Week9Day2.Auth/Controllers/HomeController.cs
+++ Week9Day2.Auth/Controllers/HomeController.cs
@@ -4,16 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Week9Day2.Auth.Models;
 
 namespace Week9Day2.Auth.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             var userId = User.Identity.GetUserId();
+            var db = new ApplicationDbContext();
+            var user = db.Users.Find(userId);
            
-            return View();
+            return View(user);
         }
 
         public ActionResult About()
